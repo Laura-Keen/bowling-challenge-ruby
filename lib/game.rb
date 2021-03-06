f9b4a2092355b9c1a39b9c1da8b346688ab5e1b0
Laura <keenlaura@hotmail.co.uk>
2021-03-06 18:22:06 +0000

    Tested base logic for frame 1

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,25 @@
 class Game
+  attr_reader :score, :frame
 
-  def score
-    7
+  def initialize
+    @score = []
+    @frame = 1
+  end
+
+  def current_frame
+    @frame
+  end
+
+  def roll_1(roll_1)
+      @score.push(roll_1)
+  end
+
+  def roll_2(roll_2)
+    @score.push(roll_2)
+    @frame = @frame + 1
+  end
+
+  def player_score
+    @score.inject(:+)
   end
 end
--- spec/frame_1_spec.rb
+++ spec/frame_1_spec.rb
@@ -4,12 +4,24 @@ describe Game do
 
   describe '#total' do
     it "should return the total score so far" do
-      frame = 1
-      roll_1 = 4
-      roll_2 = 3
-      expect(subject.score).to eq 7
+      subject.roll_1(4)
+      subject.roll_2(3)
+      expect(subject.player_score).to eq 7
+      expect(subject.current_frame).to eq 2
+    end
+
+    it "should return the total score so far" do
+      subject.roll_1(8)
+      subject.roll_2(1)
+      expect(subject.player_score).to eq 9
+      expect(subject.current_frame).to eq 2
+    end
+
+    it 'player scores a strike' do
+      subject.roll_1(10)
+      expect(subject.player_score). to eq(10)
+      expect(subject.current_frame).to eq 1
     end
   end
 
 end
-
